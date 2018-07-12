79429a2ea076f4e144ee418dadf849525c60346e
funkybo <esher5580@gmail.com>
2018-07-12 15:09:35 +0300

    init config

---
+++ src/components/Root.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Root extends Component {
+    render() {
+        return (
+            <div><h1>hello world</h1></div>
+        );
+    }
+}
+
+export default Root;
