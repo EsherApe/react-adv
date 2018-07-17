ee23108bc20bfdba696cb94ec44918e8eb610f18
funkybo <esher5580@gmail.com>
2018-07-17 10:39:25 +0300

    complex saga, remove thunk

--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import { appName } from "../config";
 import { Record } from 'immutable';
+import { all, take, call, put, cps } from 'redux-saga/effects';
 
 const ReducerRecord = Record({
   user: null,
@@ -36,30 +37,54 @@ export default function reducer(state = new ReducerRecord(), action) {
   }
 }
 
-//actions
-export function signUp(email, password) {
-  return (dispatch) => {
-    dispatch({
-      type: SIGN_UP_REQUEST
-    });
+export const signUpSaga = function* () {
+  const action = yield take(SIGN_UP_REQUEST);
+  const auth = firebase.auth();
 
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(user => dispatch({
+  while (true) {
+    try {
+      const user = yield call(
+        [auth, auth.createUserWithEmailAndPassword],
+        action.payload.email, action.payload.password
+      );
+      yield put({
         type: SIGN_UP_SUCCESS,
-        payload: {user}
-      }))
-      .catch(error => dispatch({
+        payload: user
+      })
+    } catch (error) {
+      yield put({
         type: SIGN_UP_ERROR,
         error
-      }))
+      })
+    }
+  }
+};
+
+//action creator
+export function signUp(email, password) {
+  return {
+    type: SIGN_UP_REQUEST,
+    payload: {email, password}
   }
 }
 
-firebase.auth().onAuthStateChanged(user => {
-  const store = require('../redux').default;
+//side effects with saga
+export const watchStatusChange = function* () {
+  const auth = firebase.auth();
 
-  store.dispatch({
-    type: SIGN_IN_SUCCESS,
-    payload: {user}
-  })
-});
+  try {
+    yield cps([auth, auth.onAuthStateChanged]);
+  } catch (user) {
+    yield put({
+      type: SIGN_IN_SUCCESS,
+      payload: {user}
+    })
+  }
+};
+
+export const saga = function* () {
+  yield all([
+    signUpSaga(),
+    watchStatusChange()
+  ])
+};
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,19 +1,18 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducer';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import {routerMiddleware} from 'react-router-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { routerMiddleware } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
 import history from '../history';
-import {saga} from '../ducks/people'
+import rootSaga from './saga'
 
 const sagaMiddleware = createSagaMiddleware();
-const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), thunk, logger);
+const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), logger);
 
 const store = createStore(reducer, composeWithDevTools(enhancer));
 window.store = store;
 
-sagaMiddleware.run(saga);
+sagaMiddleware.run(rootSaga);
 
 export default store;
