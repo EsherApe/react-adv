6e89f4624d4758b48fdd85898bc32286f0f2215c
funkybo <esher5580@gmail.com>
2018-07-19 17:40:38 +0300

    events selectors

--- src/ducks/events.js
+++ src/ducks/events.js
@@ -2,6 +2,8 @@ import { all, take, call, put } from 'redux-saga/effects';
 import { appName } from '../config';
 import { Record, OrderedMap } from 'immutable';
 import firebase from 'firebase';
+import { createSelector } from 'reselect'
+import { fbDataToEntities } from './utils';
 
 //Constants
 export const moduleName = 'events';
@@ -17,6 +19,16 @@ export const ReducerRecord = Record({
   loaded: false
 });
 
+export const EventRecord = Record({
+  uid: null,
+  title: null,
+  url: null,
+  where: null,
+  when: null,
+  month: null,
+  submissionDeadline: null
+});
+
 export default function reducer(state = new ReducerRecord(), action) {
   const {type, payload} = action;
 
@@ -25,16 +37,20 @@ export default function reducer(state = new ReducerRecord(), action) {
       return state.set('loading', true);
     case FETCH_ALL_SUCCESS:
       return state
-        .set('loading', true)
+        .set('loading', false)
         .set('loaded', true)
-        .set('entities', new OrderedMap(payload));
+        .set('entities', fbDataToEntities(payload, EventRecord));
     default:
       return state;
   }
 }
 
 //Selectors
-
+export const stateSelector = state => state[moduleName];
+export const entitiesSelector = createSelector(stateSelector, state => state.entities);
+export const eventListSelector = createSelector(entitiesSelector, entities => (
+  entities.valueSeq().toArray()
+));
 
 //Action creators
 export function fetchAll() {
